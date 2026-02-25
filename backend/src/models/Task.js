9c70011c0604f5d815d5b376ff2d242734a8d64a
ez-sourav <souravb2003@gmail.com>
2026-02-25 20:10:08 +0530

    Add Task model with status enum, user reference, and indexing

---
+++ backend/src/models/Task.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+const taskSchema = new mongoose.Schema({
+    title:{
+        type:String,
+        required:[true,"Task title is required"],
+        trim:true
+    },
+    description:{
+        type:String,
+        trim:true
+    },
+    status: {
+      type: String,
+      enum: ["Pending", "In Progress", "Completed"],
+      default: "Pending",
+    },
+    user:{
+        type:mongoose.Schema.ObjectId,
+        ref:"User",
+        required:true
+    }
+},{timestamps:true})
+
+taskSchema.index({ user: 1 });
+
+const Task = mongoose.model("Task", taskSchema);
+
+export default Task;

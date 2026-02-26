87f6e011d9cf8ef7efe34eb7e8dcdb52ebb9f2a5
ez-sourav <souravb2003@gmail.com>
2026-02-26 22:55:01 +0530

    Version 3 - debug render deployment

--- backend/src/controllers/taskController.js
+++ backend/src/controllers/taskController.js
@@ -3,6 +3,7 @@ import Task from "../models/Task.js";
 // Create Task
 export const createTask = async (req, res) => {
   try {
+    console.log("VERSION 3 - WITH PRIORITY FIX");
     const { title, description, status, priority } = req.body;
 
     if (!title) {

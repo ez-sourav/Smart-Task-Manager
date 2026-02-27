5ca7cf93c7f3f41635e205a48de3dbb70e73484d
ez-sourav <souravb2003@gmail.com>
2026-02-28 03:33:02 +0530

    add worng filename, now fix the file name

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerUser, loginUser } from "../controllers/authController.js";
-import { protect } from "../middleware/authMiddlerware.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
